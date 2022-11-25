96f891aa5b52f43f30b4692729cf34cfdcfebe8c
Meet Patel <meetnpatel368@gmail.com>
2022-11-24 20:30:34 -0500

    hot fix

--- src/utils/blamefileQueries/blamefileDataFetcher.js
+++ src/utils/blamefileQueries/blamefileDataFetcher.js
@@ -1,7 +1,7 @@
 import { axiosGitHubGraphQL } from "../.."
-import { lastChangeQuery } from "../repostatsQueries/_graphql"
+import { lastChangeQuery } from "./_graphql"
 
-export const fetchBlameFileData = async(ownername, reponame, branch, userid) => {
+export const fetchBlameFileData = async(ownername, reponame, branch, filepath) => {
         const res = await axiosGitHubGraphQL.post("", {
             query: lastChangeQuery(ownername, reponame, branch, filepath)
         })
