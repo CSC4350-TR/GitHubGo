89f0112f81163ff0a1eb4ecc457199e0f9fe203e
Minh Dang <minhdtq2208@gmail.com>
2022-11-30 23:11:30 +0700

    Changing body background and remove Commit Comment

--- src/components/CommitInfo.js
+++ src/components/CommitInfo.js
@@ -1,17 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 const CommitInfos = ({ selectedCommit }) => {
-  const [commentInfo, setCommentInfo] = useState([]);
-
-  useEffect(() => {
-    fetch(selectedCommit.comments_url)
-      .then((res) => res.json())
-      .then((result) => {
-        console.log("Comments");
-        console.log(result);
-        setCommentInfo(result);
-      });
-  }, [selectedCommit]);
   return (
     <div className="w-full">
       <div>
@@ -24,12 +13,6 @@ const CommitInfos = ({ selectedCommit }) => {
       <div>
         <span className="w-3 font-bold">Message: &nbsp;</span>{" "}
         {selectedCommit.commit.message || "[Blank content]"}
-      </div>
-      <div>
-        <span className="w-3 font-bold">Comments: </span>{" "}
-        {!commentInfo.length ? <i>No comments</i> : <>
-
-        </>}
       </div>
     </div>
   );
